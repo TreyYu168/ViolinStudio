0019cd0db2ac686c6eaf48b37a2699411eb858af
trey_yu168 <trey@thisorthat.com>
2021-01-10 15:33:18 -0700

    Swapped out contact page

--- assets/js/contact-us-main.js
+++ assets/js/contact-us-main.js
@@ -1,6 +1,10 @@
+function printInformation () {
+
+}
 
 (function ($) {
     "use strict";
+
 
     /*==================================================================
     [ Focus Contact2 ]*/
@@ -14,19 +18,32 @@
             }
         })    
     })
-
+  
+  
     /*==================================================================
     [ Validate ]*/
-    var input = $('.validate-input .input100');
+    var name = $('.validate-input input[name="name"]');
+    var email = $('.validate-input input[name="email"]');
+    var message = $('.validate-input textarea[name="message"]');
+
 
     $('.validate-form').on('submit',function(){
         var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
+        if($(name).val().trim() == ''){
+            showValidate(name);
+            check=false;
+        }
+
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check=false;
+        }
+
+        if($(message).val().trim() == ''){
+            showValidate(message);
+            check=false;
         }
 
         return check;
@@ -36,21 +53,8 @@
     $('.validate-form .input100').each(function(){
         $(this).focus(function(){
            hideValidate(this);
-        });
+       });
     });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
 
     function showValidate(input) {
         var thisAlert = $(input).parent();
@@ -63,6 +67,7 @@
 
         $(thisAlert).removeClass('alert-validate');
     }
+    
     
 
 })(jQuery);
